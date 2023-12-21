6aa1fb58f9f6e8d0fa385953ee33ff150d02de8b
sanketh-uta <sankethuta@gmail.com>
2023-12-21 17:01:28 -0600

    basics

---
+++ Javascript/1.Funadementals/script.js
@@ -0,0 +1,15 @@
+let country = "Bharat";
+const continent = "Asia";
+let population = 121;
+// console.log("I am from",country);
+// console.log("I belongs to",continent);
+// console.log(country,"population is",population," millions"); //if used comma as the sepeartor between the string value and variable it implicitly gives space
+
+const isIsland = false;
+let language;
+language = 'Telugu';
+console.log(isIsland);
+console.log(population);
+console.log(country);
+console.log(language);
+
